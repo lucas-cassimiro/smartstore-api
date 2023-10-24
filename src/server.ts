5bb8104d186cd9d58e061c0ae812cb9152c7a812
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-24 19:12:40 -0300

    primeira rota criada e Prisma configurado

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,34 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (req, res) => {
-    res.send("Olá mundo!");
+
+
+app.get("/:categorie", async (req, res) => {
+    const categorie = req.params.categorie;
+    try {
+        const products = await prisma.product.findMany({
+            include: {
+                colors: true,
+                storages: true,
+                categories: true,
+            },
+            where: {
+                categories: {
+                    name: {
+                        equals: categorie,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao buscar os produtos " });
+    }
 });
 
 app.listen(port, () => {
