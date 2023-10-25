37b29b1591cc5a4e4ed04ae0d6c17d2338c845e3
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-25 18:26:49 -0300

    map stocks adicionado/consertado e rota de listagem de produtos concluída

--- src/server.ts
+++ src/server.ts
@@ -5,31 +5,20 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
 
+app.get("/", async (_, res) => {
+    const products = await prisma.product.findMany({
+        include: {
+            colors: true,
+            storages: true,
+            categories: true,
+        },
+    });
 
-app.get("/:categorie", async (req, res) => {
-    const categorie = req.params.categorie;
-    try {
-        const products = await prisma.product.findMany({
-            include: {
-                colors: true,
-                storages: true,
-                categories: true,
-            },
-            where: {
-                categories: {
-                    name: {
-                        equals: categorie,
-                        mode: "insensitive",
-                    },
-                },
-            },
-        });
-        res.status(200).send(products);
-    } catch (error) {
-        return res.status(500).send({ message: "Falha ao buscar os produtos " });
-    }
+    res.json(products);
 });
+
 
 app.listen(port, () => {
     console.log(`Servidor em execucação na porta ${port}`);
