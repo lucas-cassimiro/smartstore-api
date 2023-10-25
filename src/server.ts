9aee73d1bd7a3db04e401a6a7dc1fb91c663053a
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-25 18:27:51 -0300

    rota de listagem de produto específico criada

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,36 @@ app.get("/", async (_, res) => {
     res.json(products);
 });
 
+app.get("/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const product = await prisma.product.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!product) {
+            return res.status(404).send({ message: "Produto não encontrado" });
+        }
+
+        const products = await prisma.product.findMany({
+            include: {
+                colors: true,
+                storages: true,
+                categories: true,
+            },
+            where: {
+                id,
+            },
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao filtrar produtos" });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor em execucação na porta ${port}`);
