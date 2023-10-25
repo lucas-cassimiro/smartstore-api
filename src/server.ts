b483de85a88db2a1f0a124cee10e639c612d73e6
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-25 18:29:30 -0300

    rota para cadastro de produtos criada

--- src/server.ts
+++ src/server.ts
@@ -73,6 +73,59 @@ app.get("/shop/:categorie", async (req, res) => {
     }
 });
 
+app.post("/cadastro", async (req, res) => {
+    const {
+        name,
+        price,
+        image,
+        black_friday,
+        discount,
+        average_score,
+        description,
+        created_at,
+        color_id,
+        storage_id,
+        categorie_id,
+    } = req.body;
+
+    try {
+        const productWidthSameName = await prisma.product.findFirst({
+            where: {
+                name: {
+                    equals: name,
+                    mode: "insensitive",
+                },
+            },
+        });
+
+        if (productWidthSameName) {
+            return res
+                .status(409)
+                .send({ message: "Já existe um produto cadastrado com esse nome" });
+        }
+
+        await prisma.product.create({
+            data: {
+                name: name,
+                price: price,
+                image: image,
+                black_friday: black_friday,
+                discount: discount,
+                average_score: average_score,
+                description: description,
+                created_at: new Date(created_at),
+                color_id: color_id,
+                storage_id: storage_id,
+                categorie_id: categorie_id,
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um produto " });
+    }
+
+    res.status(201).send();
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor em execucação na porta ${port}`);
