a8b4db7278c2fd0be01a46f2fa0c5811a4a934d3
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-25 18:28:57 -0300

    rota para listagem de produtos por categoria criada

--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,30 @@ app.get("/:id", async (req, res) => {
     }
 });
 
+app.get("/shop/:categorie", async (req, res) => {
+    const categorie = req.params.categorie;
+    try {
+        const products = await prisma.product.findMany({
+            include: {
+                colors: true,
+                storages: true,
+                categories: true,
+            },
+            where: {
+                categories: {
+                    name: {
+                        equals: categorie,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+        res.status(200).send(products);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao buscar os produtos " });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor em execucação na porta ${port}`);
