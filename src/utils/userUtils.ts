8348dc589ee4e1b405164aa024e5b90d699ab7f6
bielFernandes <gabrielfdias082@gmail.com>
2023-11-01 18:36:23 -0300

    started building a user utilities file

---
+++ src/utils/userUtils.ts
@@ -0,0 +1,21 @@
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
+
+
+
+// export default async function findExistentUser(user:any, buscador:any){
+    
+//     // return await prisma.user.findFirst({
+//     //     where:{
+
+//     //     }
+//     // })
+
+
+//     // return await prisma.user.findFirst({
+//     //     where:{
+//     //         email: chave
+//     //     }
+//     // })
+
+// }
