61edb68424fc5e213aff57faa1387a98add3792f
bielFernandes <gabrielfdias082@gmail.com>
2023-11-01 18:35:57 -0300

    defined typing in User Controller & build put endp

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,26 +1,51 @@
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 
+import './../utils/userUtils'
+
 const prisma = new PrismaClient();
+
+import { Request, Response } from "express";
+
+type userProps = {
+    id: number
+    email: string
+    password_hash: string
+    cpf: string
+    cellphone: string
+    first_name: string
+    last_name: string
+    date_birth: string
+}
+
 const UserController = {
-    index: async (_, res) => {
+    index: async (req:Request, res:Response) => {
+        console.log(typeof res)
+        console.log(res)
         const allUsers = await prisma.user.findMany({});
         res.json({ allUsers });
     },
 
-    create: async (req, res) => {
+    create: async (req:Request, res:Response) => {
         try {
+
+            const {email} = req.body as userProps
+
             const user = await prisma.user.findFirst({
                 where: {
-                    email: req.body.email,
+                    email,
                 },
             });
 
-            if (!user) {
+            if (user) {
+                return res.status(404).send("usuário já existe");
+            } else {
+
                 const newUser = {
-                    ...req.body,
-                    admin_auth: Boolean(true),
+                    ...req.body as userProps,
+                    admin_auth: Boolean(false),
                     created_at: new Date(),
+                    date_birth: new Date(req.body.date_birth)
                 };
 
                 const hash = bcrypt.hashSync(newUser.password_hash, 10);
@@ -32,17 +57,41 @@ const UserController = {
                 });
 
                 res.send("Objeto criado com sucesso!");
-            } else {
-                res.send("usuário já existe");
+
             }
         } catch (error) {
             console.log(error);
         }
     },
 
-    edit: (req: any, res: any) => {
-        console.log(req.body);
-        console.log(req.files);
+    edit: async (req:Request, res:Response) => {
+
+        const id = Number(req.params.id)
+
+        const existentUser = await prisma.user.findUnique({
+            where:{
+                id
+            }
+        })
+
+        if(!existentUser){
+            return res.status(404).send({message: 'Usuário não existente!'})
+        } else {
+
+            const updateUser = await prisma.user.update({
+                where:{
+                    id
+                },
+                data:{
+                    ...req.body
+                }
+            })
+    
+            console.log(updateUser)
+    
+        }
+        
+        
     },
 };
 
