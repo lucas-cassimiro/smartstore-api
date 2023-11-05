634e0c47195225a5ca3dea56c2dd9ada17e0324d
bielFernandes <gabrielfdias082@gmail.com>
2023-11-05 03:37:55 -0300

    login router

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,12 +1,15 @@
 import UserController from "../../controllers/UserController";
 import express from "express";
 import upload from "../../middleware/upload";
+import auth from "./../../middleware/auth"
 
 const userRoutes = express.Router();
 
 userRoutes.get("/", UserController.index);
 
 userRoutes.post("/", UserController.create);
+
+userRoutes.post("/login", UserController.login)
 
 userRoutes.put("/:id", upload.any(), UserController.edit);
 
