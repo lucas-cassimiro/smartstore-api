b4bb98de7988335743e7f677cff0ca798b7f6b08
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-05 17:22:03 -0300

    indexUtils indented

--- src/utils/indexUtils.ts
+++ src/utils/indexUtils.ts
@@ -1,11 +1,10 @@
-export default function identifyEmail(find: string){
+export default function identifyEmail(find: string) {
     let toggleEmail;
 
-        let includeAva = find.includes('@')
-        if(includeAva){
-            toggleEmail = true
-        }
+    const includeAva = find.includes("@");
+    if (includeAva) {
+        toggleEmail = true;
+    }
 
-        return toggleEmail ? true : false
-
+    return toggleEmail ? true : false;
 }
