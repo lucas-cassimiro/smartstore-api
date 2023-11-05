eca1e3292bf3f9741d044205b810db0d7b71b452
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-05 17:20:24 -0300

    auth indented

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,15 @@
-
 import { NextFunction, Request, Response } from "express";
 import jsonWebToken from "jsonwebtoken";
 
-const auth = (req:Request, res:Response, next:NextFunction) => {
-
-    if(req.headers.authorization){
+const auth = (req: Request, res: Response, next: NextFunction) => {
+    if (req.headers.authorization) {
         try {
-            jsonWebToken.verify(req.headers.authorization, 'segredo123')
-            next()
+            jsonWebToken.verify(req.headers.authorization, "segredo123");
+            next();
         } catch (error) {
-            res.status(401).json({ error })
+            res.status(401).json({ error });
         }
-        
-    } else res.status(401).json({ error: "Usuário não autenticado!" })
+    } else res.status(401).json({ error: "Usuário não autenticado!" });
+};
 
-}
-
-export default auth
+export default auth;
