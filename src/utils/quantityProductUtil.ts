df264e94df047845a1da591a4afb96600a7628ed
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-05 17:22:24 -0300

    quantityProductUtil typing

--- src/utils/quantityProductUtil.ts
+++ src/utils/quantityProductUtil.ts
@@ -1,38 +1,41 @@
 import { PrismaClient } from "@prisma/client";
-import identifyEmail from "./indexUtils";
+
 const prisma = new PrismaClient();
 
-export default async function deleteQuantityProduct(array:any){
+type removeQuantityInStockProps = {
+  product_id: number;
+  quantity: number;
+};
 
-    for(const item of array){
-
+export default async function removeQuantityInStock(
+    userOrder: removeQuantityInStockProps[]
+) {
+    for (const item of userOrder) {
         const findProductStock = await prisma.stock.findUnique({
-            where:{
-                product_id: item.product_id
-            }
-        })
-
+            where: {
+                product_id: item.product_id,
+            },
+        });
 
         const editProductStock = await prisma.stock.update({
-            where:{
-                product_id: item.product_id
+            where: {
+                product_id: item.product_id,
             },
-            data:{
-                quantity: Number(findProductStock?.quantity) - Number(item.quantity) 
+            data: {
+                quantity: Number(findProductStock?.quantity) - Number(item.quantity),
+            },
+        });
+        if (editProductStock) {
+            if (editProductStock.quantity! <= 0) {
+                await prisma.stock.update({
+                    where: {
+                        product_id: item.product_id,
+                    },
+                    data: {
+                        status: "Indisponível",
+                    },
+                });
             }
-        })
-        if(editProductStock){
-        if(editProductStock.quantity! <= 0){
-            await prisma.stock.update({
-                where:{
-                    product_id: item.product_id
-                },
-                data:{
-                    status: 'Indisponível'
-                }
-            })
         }
     }
-    }
-
 }
