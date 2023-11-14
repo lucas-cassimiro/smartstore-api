69d842f759276ca7a08d557316efe5c164abd784
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:58:50 -0300

    commented

--- src/routes/categories/index.ts
+++ src/routes/categories/index.ts
@@ -2,14 +2,12 @@
 
 // const router = express.Router();
 
-// import CategoriesController from "./../../controllers/CategoriesController"
+// import CategoieCo
 
 // router.get("/", CategoriesController.index);
-
 // router.post("/", CategoriesController.create);
-
 // router.put("/:id", CategoriesController.edit);
 
-// // router.delete("/:id", CategoriesController.delete);
+// router.delete("/:id", CategoriesController.delete);
 
 // export default router;
