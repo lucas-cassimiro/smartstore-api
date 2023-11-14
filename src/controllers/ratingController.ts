05a8691b66e3b90d1e14725220ce0cd21a51952a
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:58:07 -0300

    descommented

--- src/controllers/ratingController.ts
+++ src/controllers/ratingController.ts
@@ -1,41 +1,41 @@
-// import prisma from "../../config/clientPrisma";
+import prisma from "../../config/clientPrisma";
 
-// import { Request, Response } from "express";
+import { Request, Response } from "express";
 
-// const RatingController = {
-//     index: async (req: Request, res: Response) => {
-//         const ratings = await prisma.rating.findMany({
-//             include: {
-//                 products: true,
-//                 users: true,
-//             },
-//         });
+const RatingController = {
+    index: async (req: Request, res: Response) => {
+        const ratings = await prisma.rating.findMany({
+            include: {
+                products: true,
+                users: true,
+            },
+        });
 
-//         res.json(ratings);
-//     },
+        res.json(ratings);
+    },
 
-//     create: async (req: Request, res: Response) => {
-//         const { user_id, product_id, score } = req.body;
+    create: async (req: Request, res: Response) => {
+        const { user_id, product_id, score } = req.body;
 
-//         try {
-//             await prisma.rating.create({
-//                 data: {
-//                     user_id: user_id,
-//                     product_id: product_id,
-//                     score: score,
-//                     quantity: 1,
-//                 },
-//             });
-//         } catch (error) {
-//             return res
-//                 .status(500)
-//                 .send({ message: "Falha ao registrar a avaliação" });
-//         }
+        try {
+            await prisma.rating.create({
+                data: {
+                    user_id: user_id,
+                    product_id: product_id,
+                    score: score,
+                    quantity: 1,
+                },
+            });
+        } catch (error) {
+            return res
+                .status(500)
+                .send({ message: "Falha ao registrar a avaliação" });
+        }
 
-//         res.status(200).send({ message: "Avaliação registrada no banco de dados" });
-//     },
+        res.status(200).send({ message: "Avaliação registrada no banco de dados" });
+    },
 
-//     edit: async (req: Request, res: Response) => { },
-// };
+    // edit: async (req: Request, res: Response) => { },
+};
 
-// export default RatingController;
+export default RatingController;
