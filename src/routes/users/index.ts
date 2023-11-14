8ea7464d4ddaf26877cbb5202505cb06a6bd7c3b
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 17:00:16 -0300

    useroutes auth middleware added

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,13 +1,14 @@
 import userController from "../../controllers/UserController";
 import express from "express";
 import upload from "../../middleware/upload";
-//import auth from "./../../middleware/auth";
+import { authMiddleware } from "../../middleware/auth";
 
 const userRoutes = express.Router();
 
 userRoutes.get("/", userController.index);
 userRoutes.post("/", userController.create);
 userRoutes.post("/login", userController.login);
+userRoutes.get("/profile", authMiddleware, userController.profile);
 userRoutes.put("/:id", upload.any(), userController.edit);
 
 export default userRoutes;
