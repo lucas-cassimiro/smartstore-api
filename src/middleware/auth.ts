a32513a55b73b7750ff282055e565ea49724c4c5
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:58:32 -0300

    new authts middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,15 +1,57 @@
 import { NextFunction, Request, Response } from "express";
-import jsonWebToken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
-    if (req.headers.authorization) {
-        try {
-            jsonWebToken.verify(req.headers.authorization, "segredo123");
-            next();
-        } catch (error) {
-            res.status(401).json({ error });
+import prisma from "../../config/clientPrisma";
+
+// type JwtPayload = {
+//   email: string;
+// };
+
+export const authMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { authorization } = req.headers;
+
+        console.log("autorização:", authorization);
+
+        if (!authorization) {
+            return res.status(401).send({ message: "Não autorizado" });
         }
-    } else res.status(401).json({ error: "Usuário não autenticado!" });
-};
 
-export default auth;
+        const token = authorization.split(" ")[1];
+
+        console.log(token);
+
+        const { id } = (
+      jwt.verify(token, process.env.JWT_PASS ?? "") as {
+        data: { id: number };
+      }
+        ).data;
+
+        const findUser = await prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!findUser) {
+            return res.status(401).send({ message: "Não autorizado." });
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { password_hash: _, ...loggedUser } = findUser;
+
+        req.user = loggedUser;
+
+        console.log("loggedUser:", loggedUser);
+        console.log("reqUser:", req.user);
+
+        next();
+    } catch (error) {
+        console.log("Erro ao decodificar o token:", error);
+        return res.status(500).send({ message: "Não autorizado." });
+    }
+};
