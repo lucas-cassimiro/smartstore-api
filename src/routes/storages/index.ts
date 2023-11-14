5733aa239e00bcd3aee3df4db08a742ae79688a2
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:59:46 -0300

    descommented

--- src/routes/storages/index.ts
+++ src/routes/storages/index.ts
@@ -1,11 +1,11 @@
-// import express from "express";
+import express from "express";
 
-// import StorageController from "../../controllers/StorageController";
+import StorageController from "../../controllers/storageController";
 
-// const storageRoutes = express.Router();
+const storageRoutes = express.Router();
 
-// storageRoutes.get("/", StorageController.index);
-// storageRoutes.post("/", StorageController.create);
-// storageRoutes.put("/:id", StorageController.edit);
+storageRoutes.get("/", StorageController.index);
+storageRoutes.post("/", StorageController.create);
+storageRoutes.put("/:id", StorageController.edit);
 
-// export default storageRoutes;
+export default storageRoutes;
