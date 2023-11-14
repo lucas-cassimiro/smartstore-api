74556a7d1a041cafbe17917caf612d1ffa5a0de2
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:59:33 -0300

    descommented

--- src/routes/ratings/index.ts
+++ src/routes/ratings/index.ts
@@ -1,11 +1,11 @@
-// import express from "express";
+import express from "express";
 
-// import RatingController from "../../controllers/RatingController";
+import RatingController from "../../controllers/ratingController";
 
-// const ratingRoutes = express.Router();
+const ratingRoutes = express.Router();
 
-// ratingRoutes.get("/", RatingController.index);
-// ratingRoutes.post("/", RatingController.create);
+ratingRoutes.get("/", RatingController.index);
+ratingRoutes.post("/", RatingController.create);
 // ratingRoutes.put("/:id", RatingController.edit);
 
-// export default ratingRoutes;
+export default ratingRoutes;
