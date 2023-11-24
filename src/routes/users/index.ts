e932eacb5c243bbdc4f103067d619fc2db208a85
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 20:18:25 -0300

    new userController

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,13 +1,14 @@
-import userController from "../../controllers/UserController";
 import express from "express";
+
 import { authMiddleware } from "../../middleware/authMiddleware";
+import { UserController } from "../../controllers/UserController";
 
 const userRoutes = express.Router();
 
-userRoutes.get("/", userController.index);
-userRoutes.post("/", userController.create);
-userRoutes.post("/login", userController.login);
-userRoutes.get("/profile", authMiddleware, userController.profile);
-userRoutes.put("/:id", userController.edit);
+userRoutes.get("/", new UserController().getUsers);
+userRoutes.get("/profile", authMiddleware, new UserController().profile);
+userRoutes.post("/", new UserController().create);
+userRoutes.post("/login", new UserController().login);
+userRoutes.put("/:id", new UserController().edit);
 
 export default userRoutes;
