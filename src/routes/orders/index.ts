db290311bdf66023a0569c31e0df0619a19436d8
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 19:37:04 -0300

    orderRoutes add get param id

--- src/routes/orders/index.ts
+++ src/routes/orders/index.ts
@@ -5,6 +5,7 @@ import OrderController from "../../controllers/orderController";
 const orderRoutes = express.Router();
 
 orderRoutes.get("/", OrderController.index);
+orderRoutes.get("/:id", OrderController.indexParam);
 orderRoutes.post("/", OrderController.create);
 
 export default orderRoutes;
