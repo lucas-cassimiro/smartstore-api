2d8449c12c52159e7c858b7841e5254fe1d80490
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 20:17:19 -0300

    routes newOrderController

--- src/routes/orders/index.ts
+++ src/routes/orders/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
-import OrderController from "../../controllers/orderController";
+import { OrderController } from "../../controllers/OrderController";
 
 const orderRoutes = express.Router();
 
-orderRoutes.get("/", OrderController.index);
-orderRoutes.get("/:id", OrderController.indexParam);
-orderRoutes.post("/", OrderController.create);
+orderRoutes.get("/", new OrderController().getOrder);
+orderRoutes.get("/:id", new OrderController().getOrderParam);
+orderRoutes.post("/", new OrderController().create);
 
 export default orderRoutes;
