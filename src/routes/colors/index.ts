ced2e12cb7b70c00bc4d8af03c08b6043b1de2f1
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 20:16:58 -0300

    color routes for new Color Controller

--- src/routes/colors/index.ts
+++ src/routes/colors/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
-import ColorController from "../../controllers/colorController";
+import { ColorController } from "../../controllers/ColorController";
 
 const colorRoutes = express.Router();
 
-colorRoutes.get("/", ColorController.index);
-colorRoutes.post("/", ColorController.create);
-colorRoutes.put("/:id", ColorController.edit);
+colorRoutes.get("/", new ColorController().getColor);
+colorRoutes.post("/", new ColorController().create);
+colorRoutes.put("/:id", new ColorController().edit);
 
 export default colorRoutes;
