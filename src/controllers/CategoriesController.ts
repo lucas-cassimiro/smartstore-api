e5dd5322c84d16d84a6fd98c6ff0444505826579
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 19:35:14 -0300

    categoriesController fixed up

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -1,75 +1,85 @@
-// import prisma from "../../config/clientPrisma";
+import { Request, Response } from "express";
 
-// import { Request, Response } from "express";
-// import findExistentItem from "../utils/index/findExistentItem";
+import prisma from "../../config/clientPrisma";
 
-// const CategoriesController = {
+//import findExistentItem from "../utils/index/findExistentItem";
 
-//     index: async (req: Request, res: Response) => {
-//         const categories = await prisma.categorie.findMany({});
-//         res.json(categories);
-//     },
+export class CategorieController {
+    async getCategorie(req: Request, res: Response) {
+        const categories = await prisma.categorie.findMany({});
+        res.json(categories);
+    }
 
-//     create: async (req: Request, res: Response) => {
-//         const { name } = req.body;
-//         try {
-//             const categorieExistentInDatabase = await findExistentItem(name)
+    async create(req: Request, res: Response) {
+        const { name } = req.body;
+        try {
+            const categorieExistentInDatabase = await prisma.categorie.findUnique({
+                where: {
+                    name,
+                },
+            });
 
-//             if (categorieExistentInDatabase) {
-//                 return res
-//                     .status(404)
-//                     .send({ message: "Categoria de produtos já existe na base de dados" });
-//             }
+            if (categorieExistentInDatabase) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Categoria de produtos já existe na base de dados",
+                    });
+            }
 
-//             await prisma.categorie.create({
-//                 data: {
-//                     name,
-//                 },
-//             });
-//         } catch (error) {
-//             return res
-//                 .status(404)
-//                 .send({ message: "Erro ao cadastrar nova categoria de produtos" });
-//         }
+            await prisma.categorie.create({
+                data: {
+                    name,
+                },
+            });
+        } catch (error) {
+            return res
+                .status(404)
+                .send({ message: "Erro ao cadastrar nova categoria de produtos" });
+        }
 
-//         res.status(200).send({
-//             message: "Nova categoria de produtos cadastrada na base de dados",
-//         });
-//     },
+        res.status(200).send({
+            message: "Nova categoria de produtos cadastrada na base de dados",
+        });
+    }
 
-//     edit: async (req: Request, res: Response) => {
-//         const { name } = req.body;
-//         const { id } = req.body;
+    async edit(req: Request, res: Response) {
+        const { name } = req.body;
+        const { id } = req.body;
 
-//         try {
-//             const categorieExistentInDatabase = await findExistentItem(id)
+        try {
+            const categorieExistentInDatabase = await prisma.categorie.findUnique({
+                where: {
+                    id,
+                },
+            });
 
-//             if (!categorieExistentInDatabase) {
-//                 return res
-//                     .status(400)
-//                     .send({ message: "Categoria de produto não consta na base de dados " });
-//             };
+            if (!categorieExistentInDatabase) {
+                return res
+                    .status(400)
+                    .send({
+                        message: "Categoria de produto não consta na base de dados ",
+                    });
+            }
 
-//             await prisma.categorie.update({
-//                 where: {
-//                     id,
-//                 },
-//                 data: {
-//                     name,
-//                 },
-//             });
+            await prisma.categorie.update({
+                where: {
+                    id,
+                },
+                data: {
+                    name,
+                },
+            });
+        } catch (error) {
+            return res
+                .status(404)
+                .send({
+                    message: "Falha ao cadastrar nova categoria na base de dados",
+                });
+        }
 
-//         } catch (error) {
-//             return res
-//                 .status(404)
-//                 .send({ message: "Falha ao cadastrar nova categoria na base de dados" });
-//         }
-
-//         return res
-//             .status(200)
-//             .send({ message: "Categoria de produto alterada na base de dados " });
-//     }
-//     }
-// }
-
-// export default CategoriesController;
+        return res
+            .status(200)
+            .send({ message: "Categoria de produto alterada na base de dados " });
+    }
+}
