6f53b7ce48cfd6d8402b176b4f607c22eb47ef6d
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-24 20:17:29 -0300

    new ProductController

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
 
-import productController from "../../controllers/productController";
+import { ProductController } from "../../controllers/ProductController";
 
 import upload from "../../middleware/multerProductImages";
 
 const productsRoutes = express.Router();
 
-productsRoutes.get("/", productController.index);
-productsRoutes.get("/:param", productController.indexParam);
-productsRoutes.post("/", upload.single("file"), productController.create);
-productsRoutes.put("/:id", productController.edit);
-productsRoutes.delete("/:id", productController.delete);
+productsRoutes.get("/", new ProductController().getProducts);
+productsRoutes.get("/:param", new ProductController().getProductsParam);
+productsRoutes.post("/", upload.single("file"), new ProductController().create);
+productsRoutes.put("/:id", new ProductController().edit);
+productsRoutes.delete("/:id", new ProductController().delete);
 
 export default productsRoutes;
