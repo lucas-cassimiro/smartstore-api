38a1c427240f18a45f16a39e0bcf4054cbf0e7b2
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-25 20:33:55 -0300

    /app

---
+++ src/app/controllers/OrderItemsController.ts
@@ -0,0 +1,16 @@
+import prisma from "../../../config/clientPrisma";
+
+import { Request, Response } from "express";
+
+export class OrderItemsController {
+    async index(req: Request, res: Response) {
+        const orderItems = await prisma.order_item.findMany({
+            include: {
+                products: true,
+                orders: true,
+            },
+        });
+
+        res.json(orderItems);
+    }
+}
