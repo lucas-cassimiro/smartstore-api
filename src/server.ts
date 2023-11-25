3213263b11bdf8fc0dbb0f137e2598d125964a51
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-25 20:33:06 -0300

    server.ts altered

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,8 @@
-import express from "express";
-
-import cors from "cors";
-
 import "dotenv/config";
+import app from "./app";
 
-import routerUser from "./routes/users";
-import routerProducts from "./routes/products";
-import routerStock from "./routes/stocks";
-import routerColors from "./routes/colors";
-import routerStorages from "./routes/storages";
-import routerRatings from "./routes/ratings";
-import routerOrderItems from "./routes/orderItems";
-import routerAddress from "./routes/addresses";
-import routerCategories from "./routes/categories";
-//import routerOrders from "./routes/orders";
-
-const port = 3001;
-const app = express();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use("/users", routerUser);
-app.use("/produto", routerProducts);
-app.use("/estoque", routerStock);
-app.use("/cor", routerColors);
-app.use("/armazenamento", routerStorages);
-app.use("/ratings", routerRatings);
-app.use("/pedidos", routerOrderItems);
-app.use("/endereco", routerAddress);
-app.use("/categoria", routerCategories);
-
-//app.use("/pedidosorder", routerOrders);
+const port = 3333;
 
 app.listen(port, () => {
-    console.log(`Servidor em execucação na porta ${port}`);
+    console.log(`Servidor em execução na porta ${port}`);
 });
