92bef72def3b16950df07692aefce06443eb2795
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-26 15:38:08 -0300

    interface ProductData

---
+++ src/interfaces/ProductData.ts
@@ -0,0 +1,14 @@
+import { StockData } from "./StockData";
+
+export interface ProductData extends StockData {
+  name: string;
+  price: number;
+  black_friday: boolean;
+  discount: number;
+  average_score?: number;
+  description: string;
+  color_id: number;
+  storage_id: number;
+  categorie_id: number;
+  ean: string;
+}
