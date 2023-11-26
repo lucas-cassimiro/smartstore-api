93e537fdd9d9b66c0acf9f18cb7c0de7a3b91bf5
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-26 15:37:41 -0300

    interface AddressData

---
+++ src/interfaces/AddressData.ts
@@ -0,0 +1,10 @@
+export interface AddressData {
+  user_id: number;
+  street_address: string;
+  number_address: number;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  recipient: string;
+}
