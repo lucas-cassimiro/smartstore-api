51aa96c0e96cb212459a870f65aebcaee2bc7f71
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 12:59:03 -0300

    expiry date added typing

--- src/interfaces/StockData.ts
+++ src/interfaces/StockData.ts
@@ -2,7 +2,7 @@ export interface StockData {
   product_id: number;
   status: string;
   purchase_price: number;
-  expiry_date?: Date;
+  expiry_date: Date | null;
   updated_at: Date;
   quantity: number;
 }
