6b6cc1e247702a5b1deb32a34473c21ac5b2ea2e
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 12:58:13 -0300

    try catch add index

--- src/app/controllers/StocksController.ts
+++ src/app/controllers/StocksController.ts
@@ -5,25 +5,31 @@ import { StockData } from "../../interfaces/StockData";
 
 export class StockController {
     async index(_req: Request, res: Response) {
-        const productsInStock = await prisma.stock.findMany({
-            include: {
-                products: {
-                    include: {
-                        colors: true,
-                        storages: true,
-                        categories: true,
+        try {
+            const productsInStock = await prisma.stock.findMany({
+                include: {
+                    products: {
+                        include: {
+                            colors: true,
+                            storages: true,
+                            categories: true,
+                        },
                     },
                 },
-            },
-        });
+            });
 
-        return res.json(productsInStock);
+            return res.json(productsInStock);
+        } catch (error) {
+            return res
+                .status(500)
+                .send({ message: "Falha ao buscar estoque dos produtos." });
+        }
     }
 
     async update(req: Request, res: Response) {
         const product_id: number = Number(req.params.id);
 
-        const { status, purchase_price, expiry_date, updated_at, quantity } =
+        const { status, purchase_price, expiry_date, quantity } =
       req.body as StockData;
 
         try {
@@ -36,10 +42,12 @@ export class StockController {
             if (!product) {
                 return res
                     .status(404)
-                    .send({ message: "Produto não existe na base de dados" });
+                    .send({ message: "Produto não existe na base de dados." });
             }
 
             const quantidadeAtual: number = product.quantity;
+
+            const expiryDate = expiry_date ? new Date(expiry_date) : undefined;
 
             await prisma.stock.update({
                 where: {
@@ -48,21 +56,20 @@ export class StockController {
                 data: {
                     status: status,
                     purchase_price: purchase_price,
-                    expiry_date:
-            expiry_date !== undefined ? new Date(expiry_date) : undefined,
-                    updated_at: new Date(updated_at),
+                    expiry_date: expiryDate,
+                    updated_at: new Date(),
                     quantity: quantidadeAtual! + Number(quantity),
                 },
             });
 
             return res
                 .status(200)
-                .send({ message: "Produto atualizado com sucesso" });
+                .send({ message: "Produto atualizado com sucesso." });
         } catch (error) {
             console.log(error);
             return res
                 .status(500)
-                .send({ message: "Falha ao atualizar o estoque do produto" });
+                .send({ message: "Falha ao atualizar o estoque do produto." });
         }
     }
 }
